c42c8a1fcd471f9a275e0675b18574aeb0b78004
Andrew Chiw <randomshinichi4869@gmail.com>
2021-06-10 17:02:16 +0200

    instantiate Value Token with interface; DORCP is an escrow contract, we don't need the escrow contract anymore

--- src/deploy.ts
+++ src/deploy.ts
@@ -2,13 +2,13 @@ import { config } from 'dotenv';
 import * as fs from 'fs';
 import { DirectSecp256k1HdWallet } from "@cosmjs/proto-signing";
 import { SigningCosmWasmClient } from "@cosmjs/cosmwasm-stargate";
+import { CW20 } from "./cw20-base-helpers"
 
 config();
 
 const { ERC20_CONTRACT, ESCROW_CONTRACT, DORIUM_PROPOSAL_CONTRACT, MNEMONIC_MAIN, RPC_ENDPOINT } = process.env;
 const options = { prefix: "wasm" };
 const ERC20Contract = fs.readFileSync(ERC20_CONTRACT)
-const EscrowContract = fs.readFileSync(ESCROW_CONTRACT);
 const ProposalContract = fs.readFileSync(DORIUM_PROPOSAL_CONTRACT);
 
 async function getWalletData() {
@@ -40,33 +40,24 @@ async function instantiateCW20() {
 		}
 	}
 	const contractData = await client.upload(account.address, ERC20Contract)
-	const contractAddress = await client.instantiate(account.address, contractData.codeId, initMsg, "creating the cw20 token")
+	const instantiateData = await client.instantiate(account.address, contractData.codeId, initMsg, "creating the cw20 token")
 	console.log('CW20 Value Token Uploaded Contract', contractData);
-	console.log('CW20 Value Token Instantiated Contract', contractAddress);
+	console.log('CW20 Value Token Instantiated Contract', instantiateData);
+	const cw20 = CW20(client).use(instantiateData.contractAddress)
 }
 
-async function uploadEscrow() {
-	const account = await getWalletAccount();
-	const wallet = await getWalletData();
-	const client = await SigningCosmWasmClient.connectWithSigner(RPC_ENDPOINT, wallet, options);
-	const contractData = await client.upload(account.address, EscrowContract)
-	console.log('Escrow', contractData);
-}
-
-
-async function uploadProposal() {
+async function uploadDoriumCommunityProposal() {
 	const account = await getWalletAccount();
 	const wallet = await getWalletData();
 	const client = await SigningCosmWasmClient.connectWithSigner(RPC_ENDPOINT, wallet, options);
 	const contractData = await client.upload(account.address, ProposalContract)
-	console.log('DORCP', contractData);
+	console.log('DORium Community Proposal', contractData);
 }
 
 export async function main() {
 	try {
 		await instantiateCW20();
-		await uploadEscrow();
-		await uploadProposal();
+		await uploadDoriumCommunityProposal();
 	} catch (e) {
 		throw e;
 	}
