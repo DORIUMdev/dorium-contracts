078b5a7763c0a81c6cbe82d39a6d77399684d7b0
Andrew Chiw <randomshinichi4869@gmail.com>
2021-06-23 11:50:55 +0200

    refactor: separated uploading from instantiation, save state of uploaded contracts in contracts.json

--- src/deploy.ts
+++ src/deploy.ts
@@ -1,8 +1,8 @@
 import { config } from 'dotenv';
 import * as fs from 'fs';
-import { DirectSecp256k1HdWallet } from '@cosmjs/proto-signing';
-import { SigningCosmWasmClient } from '@cosmjs/cosmwasm-stargate';
-// import { CW20 } from "./cw20-base-helpers"
+import { AccountData, DirectSecp256k1HdWallet } from '@cosmjs/proto-signing';
+import { SigningCosmWasmClient, UploadResult } from '@cosmjs/cosmwasm-stargate';
+import { CW20 } from './cw20-base-helpers';
 
 config();
 
@@ -22,10 +22,18 @@ async function getWalletAccount() {
 	return mainAccount;
 }
 
-async function instantiateCW20() {
-	const account = await getWalletAccount();
-	const wallet = await getWalletData();
-	const client = await SigningCosmWasmClient.connectWithSigner(RPC_ENDPOINT, wallet, options);
+async function uploadContracts(account: AccountData, wallet: DirectSecp256k1HdWallet, client: SigningCosmWasmClient) {
+	const con_cw20 = await client.upload(account.address, ERC20Contract);
+	console.log("CW20 Uploaded Contract", con_cw20);
+	const con_dorcp = await client.upload(account.address, ProposalContract);
+	console.log("DORCP Uploaded Contract", con_dorcp);
+	var contracts = {
+		cw20: {codeId: con_cw20.codeId, transactionHash: con_cw20.transactionHash},
+		dorcp: {codeId: con_dorcp.codeId, transactionHash: con_dorcp.transactionHash},
+	}
+	return contracts
+}
+async function instantiateCW20(contractData: UploadResult, account: AccountData, wallet: DirectSecp256k1HdWallet, client: SigningCosmWasmClient) {
 	const initMsg = {
 		name: 'Dorium Value Token',
 		symbol: 'TREE',
@@ -37,30 +45,35 @@ async function instantiateCW20() {
 			minter: 'wasm1ryuawewrukex42yh2kpydtpdh90ex096kaajek',
 		},
 	};
-	const contractData = await client.upload(account.address, ERC20Contract);
+
+	const instanceData = await client.instantiate(account.address, contractData.codeId, initMsg, "instantiating the DORCP contract");
+	return instanceData
+}
+
+async function instantiateDoriumCommunityProposal(contractData: UploadResult, account: AccountData, wallet: DirectSecp256k1HdWallet, client: SigningCosmWasmClient) {
 	const instantiateData = await client.instantiate(
 		account.address,
 		contractData.codeId,
-		initMsg,
+		{},
 		'creating the cw20 token'
 	);
-	console.log('CW20 Value Token Uploaded Contract', contractData);
-	console.log('CW20 Value Token Instantiated Contract', instantiateData);
-	// const cw20 = CW20(client).use(instantiateData.contractAddress)
-}
-
-async function uploadDoriumCommunityProposal() {
-	const account = await getWalletAccount();
-	const wallet = await getWalletData();
-	const client = await SigningCosmWasmClient.connectWithSigner(RPC_ENDPOINT, wallet, options);
-	const contractData = await client.upload(account.address, ProposalContract);
-	console.log('DORium Community Proposal', contractData);
+	return instantiateData
 }
 
 export async function main() {
 	try {
-		await instantiateCW20();
-		await uploadDoriumCommunityProposal();
+		const account = await getWalletAccount();
+		const wallet = await getWalletData();
+		const client = await SigningCosmWasmClient.connectWithSigner(RPC_ENDPOINT, wallet, options);
+
+		// const contracts = uploadContracts(account, wallet, client);
+		// fs.writeFileSync("contracts.json", JSON.stringify(contracts))
+
+		var con = JSON.parse(fs.readFileSync("contracts.json").toString());
+		const inst_cw20 = await instantiateCW20(con.cw20, account, wallet, client);
+		console.log("CW20 Instantiated", inst_cw20);
+		const inst_dorcp = await instantiateDoriumCommunityProposal(con.dorcp, account, wallet, client);
+		console.log("DORCP Instantiated", inst_dorcp);
 	} catch (e) {
 		throw e;
 	}
