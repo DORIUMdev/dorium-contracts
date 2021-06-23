8b6575fc8d73f29fd0bdd6895740b19a1fade6b4
Ievgenii Karakulin <eugene@apeunit.com>
2021-06-23 10:40:39 +0300

    Wallet init impl using Keplr extension

--- wasmdstart.sh
+++ wasmdstart.sh
@@ -11,6 +11,9 @@ wasmd init localnet --chain-id ${CHAIN_ID} --home ${APP_HOME}
 # add minimum gas prices config to app configuration file
 sed -i -r 's/minimum-gas-prices = ""/minimum-gas-prices = "0.01ucosm"/' ${APP_HOME}/config/app.toml
 
+# resolve CORS policy blocking during request to the chain by the Keplr API
+sed -i -r 's/cors_allowed_origins = \[\]/cors_allowed_origins = \["*"\]/' ${APP_HOME}/config/config.toml
+
 # add your wallet addresses to genesis
 wasmd add-genesis-account $(wasmd keys show -a main --keyring-backend=test) 10000000000ucosm,10000000000stake --home ${APP_HOME}
 wasmd add-genesis-account $(wasmd keys show -a validator --keyring-backend=test) 10000000000ucosm,10000000000stake --home ${APP_HOME}
