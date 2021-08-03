2e453e5d6b9da9182cb149979f8271b86dffd772
Andrew Chiw <randomshinichi4869@gmail.com>
2021-08-03 19:01:08 +0200

    get top level async function working

--- index.ts
+++ index.ts
@@ -1,11 +1,9 @@
 import { deploy, uploadContracts2, scratchpad} from "./src/deploy";
-
-void async function main() {
+(async () => {
     if(process.argv[2] == "deploy") {
         await uploadContracts2()
         await deploy()
     } else if(process.argv[2] == "custom") {
         await scratchpad()
     }
-
-}
+})();
