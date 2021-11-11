d30745f1ebdb206d2a247c8feb2a1939343f3bc9
Andrew Chiw <randomshinichi4869@gmail.com>
2021-11-11 15:07:10 +0100

    changed denomination from ucosm to udor, decrease minimum gas prices by 10x

--- wasmdstart.sh
+++ wasmdstart.sh
@@ -9,7 +9,7 @@ rm -rf $APP_HOME
 wasmd init localnet --chain-id ${CHAIN_ID} --home ${APP_HOME}
 
 # add minimum gas prices config to app configuration file
-sed -i -r 's/minimum-gas-prices = ""/minimum-gas-prices = "0.01ucosm"/' ${APP_HOME}/config/app.toml
+sed -i -r 's/minimum-gas-prices = ""/minimum-gas-prices = "0.001udor"/' ${APP_HOME}/config/app.toml
 
 # enable 1317 API
 perl -0777 -i.original -pe 's/# Enable defines if the API server should be enabled.\nenable = false/# Enable defines if the API server should be enabled.\nenable = true/igs' ${APP_HOME}/config/app.toml
@@ -18,8 +18,8 @@ perl -0777 -i.original -pe 's/# Enable defines if the API server should be enabl
 sed -i -r 's/cors_allowed_origins = \[\]/cors_allowed_origins = \["*"\]/' ${APP_HOME}/config/config.toml
 
 # add your wallet addresses to genesis
-wasmd add-genesis-account $(wasmd keys show -a main --keyring-backend=test) 10000000000ucosm,10000000000stake --home ${APP_HOME}
-wasmd add-genesis-account $(wasmd keys show -a validator --keyring-backend=test) 10000000000ucosm,10000000000stake --home ${APP_HOME}
+wasmd add-genesis-account $(wasmd keys show -a main --keyring-backend=test) 10000000000udor,10000000000stake --home ${APP_HOME}
+wasmd add-genesis-account $(wasmd keys show -a validator --keyring-backend=test) 10000000000udor,10000000000stake --home ${APP_HOME}
 
 # add fred's address as validator's address
 wasmd gentx validator 1000000000stake --home ${APP_HOME} --chain-id ${CHAIN_ID} --keyring-backend=test --keyring-dir=~/.wasmd
